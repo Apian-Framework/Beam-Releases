37f0ae448d27418d82a3f084140c8a4c0b403e11
jim berry <jim@bigfattail.com>
2023-01-11 15:25:59 -0500

    tools

--- Release-Repo-Tools/tagandpush.bat
+++ Release-Repo-Tools/tagandpush.bat
@@ -1,4 +1,13 @@
 @echo off
+rem
+rem Creates and pushes a new "release"
+rem Assumes all of the local submodules are as they should be for the release.
+rem
+rem When run with a name for the release,  this script first creates a tag with that name
+rem in each of the submodules' repositories, and then pushes them to github
+rem Then the script creates a new *branch* with that name in the current repo and
+rem pushes the branch.
+
 
 set RELEASE_TAG=%1
 if  "%RELEASE_TAG%" == "" (
@@ -7,6 +16,7 @@ if  "%RELEASE_TAG%" == "" (
 )
 
 rem check for git lfs support
+rem LFS is only necessary because the Beam.Unity repo contains big binary resource files.
 echo Checking for git Large File Storage support...
 for /f "delims=" %%a in ('git lfs version^|findstr /B /R /C:"^git-lfs/[0-9\.]* (.*)"') do @set lfsresult=%%a
 if  "%lfsresult%" == "" (
@@ -15,22 +25,14 @@ if  "%lfsresult%" == "" (
 )
 echo LFS installed.
 
-rem checkout "main" for all the submodules
-rem and then pull from origin
-echo Updating all submosules to current "main" branch
-git submodule update
-git submodule foreach "git checkout main"
-git submodule foreach "git pull --no-rebase"
-if %ERRORLEVEL% neq 0 (
-  echo Git pull failed. Fix the problem and try again.
-  exit /b 1
-)
+echo \nTagging current submodules as %RELEASE_TAG%
+rem git submodule foreach "git tag %RELEASE_TAG%"
 
-echo Tagging current version as %RELEASE_TAG%
-git submodule foreach "git tag %RELEASE_TAG%"
-git submodule foreach "git push --tags"
+echo \nPushing tagged submodules
+rem git submodule foreach "git push --tags"
 
-git tag %RELEASE_TAG%
+rem echo \nCreating and pushing release branch  %RELEASE_TAG%
+git checkout -b %RELEASE_TAG%
 git push --tags
 
 
